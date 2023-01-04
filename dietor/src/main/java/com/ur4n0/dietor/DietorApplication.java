25b85d44487dd493faaea30ebdfd067f5f5d39cc
Matheus Fernandes <matheus.fernandes92.235@protonmail.com>
2023-01-03 22:35:06 -0300

    initial commit

---
+++ dietor/src/main/java/com/ur4n0/dietor/DietorApplication.java
@@ -0,0 +1,13 @@
+package com.ur4n0.dietor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietorApplication.class, args);
+	}
+
+}
---
+++ dietor/src/test/java/com/ur4n0/dietor/DietorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ur4n0.dietor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
